855a20425b87aa6ade50a58e67f9281fe737f9cc
Your Name <you@example.com>
2019-06-14 12:47:08 +0900

    winner line highlight update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
   function Square(props){
       return (
-        <button className="square" onClick={props.onClick}>
+        <button className={"square " + props.className } onClick={props.onClick}>
             {props.value}
         </button>
       );
@@ -16,6 +16,7 @@ import './index.css';
       return ( 
         <Square
             value={this.props.squares[i]}
+            className={this.props.winline.find((a,b,c) => a===i ) ? "winline" : ""}
             onClick={() => this.props.onClick(i)}
         />
       );
@@ -55,16 +56,18 @@ import './index.css';
             stepNumber: 0,
             xIsNext: true,
             order: true,
+            winline: Array(3).fill(null),
         };
     }
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
+        if (this.state.winline[0] != null || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+        const wins = calculateWinner(squares);
         this.setState({
             history: history.concat([{
                 squares: squares,
@@ -72,6 +75,7 @@ import './index.css';
             }]),
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+            winline: wins ? wins.winline : Array(3).fill(null),
         });
     }
 
@@ -91,14 +95,14 @@ import './index.css';
     render() {
       const history = this.state.history;
       const current = history[this.state.stepNumber];
-      const winner = calculateWinner(current.squares);
+      const wins = calculateWinner(current.squares);
 
       const moves = history.map((step, move) => {
           const desc = move ?
             'Go to move #' + move + '(' + spuarePoint(step.psquare) + ')' :
             'Go to game start';
           // 太字クラス「activelist」
-          const name = (current.squares == step.squares) ?
+          const name = (current.squares === step.squares) ?
             'activelist' :
             ' ';
           return (
@@ -109,8 +113,8 @@ import './index.css';
       });
 
       let status;
-      if (winner) {
-          status = 'Winner: ' + winner;
+      if (wins) {
+          status = 'Winner: ' + wins.winner;
       } else {
           status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
       }
@@ -133,6 +137,7 @@ import './index.css';
           <div className="game-board">
             <Board 
                 squares={current.squares}
+                winline={this.state.winline}
                 onClick={(i) => this.handleClick(i)}
             />
 
@@ -167,22 +172,26 @@ import './index.css';
       return(points[square]);
 
   }
+
   function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return {
+          winner: squares[a],
+          winline: lines[i],
+        };
+      }
     }
+    return null;
   }
-  return null;
-}
